3636cc4daddfd6317ba31568d0e19c4418ff48e6
Lorenz Possnig <lorenz.possnig@gmail.com>
2022-11-20 14:37:29 +0100

    Add files via upload

---
+++ ex1_solution.cu
@@ -0,0 +1,90 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<cuda.h>
+
+#define BLOCK_SIZE 256
+#define ARRAY_SIZE 16777216
+
+float sum(int n, float *f) {
+    float res = 0.0f;
+    for(int i = 0; i < n; i++) {
+        res += f[i];
+    }
+    return res;
+}
+
+void cpu_saxpy(int n, float a, float *x, float *y) {
+    for (int i = 0; i < n; i++) {
+        y[i] = a * x[i] + y[i];
+    }
+}
+
+// Declare a kernel gpu_saxpy with the same args as cpu_saxpy
+__global__ void gpu_saxpy(int n, float a, float *x, float *y) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = idx; i < n; i += stride) {
+        y[i] = a * x[i] + y[i];
+    }
+}
+
+double getMillis(time_t t1, time_t t2) {
+    return (double)(t2 - t1) * 1000.0L;
+}
+
+int main() {
+
+    float a = 2.0f;
+    // Allocate arrays x and y with size ARRAY_SIZE
+    float *x = (float*) malloc(sizeof(float) * ARRAY_SIZE);
+    float *y = (float*) malloc(sizeof(float) * ARRAY_SIZE);
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        x[i] = 0.1f;
+        y[i] = 0.2f;
+    }
+
+    // Allocate arrays d_x and d_y and copy over content from host memory
+    float *d_x, *d_y;
+    cudaMalloc((void**)&d_x, sizeof(float) * ARRAY_SIZE);
+    cudaMalloc((void**)&d_y, sizeof(float) * ARRAY_SIZE);
+    cudaMemcpy(d_x, x, sizeof(float) * ARRAY_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, sizeof(float) * ARRAY_SIZE, cudaMemcpyHostToDevice);
+
+    time_t t1 = clock();
+
+    cpu_saxpy(ARRAY_SIZE, a, x, y);
+    float error = sum(ARRAY_SIZE, y);
+    
+    time_t t2 = clock();
+    
+    // start your kernel here
+    // make sure your execution configuration is correct
+    int blocks = (ARRAY_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    gpu_saxpy<<<blocks, BLOCK_SIZE>>>(ARRAY_SIZE, a, d_x, d_y);
+    time_t t3 = clock();
+    // copy the gpu memory back to the host
+    cudaMemcpy(y, d_y, sizeof(float) * ARRAY_SIZE, cudaMemcpyDeviceToHost);
+    
+    time_t t4 = clock();
+
+    error = fabsf(error - sum(ARRAY_SIZE, y));
+    if (error > 0.0001f) {
+        printf("Error: The solution is not correct\n");
+    } else {
+        double cpuTime = getMillis(t1, t2);
+        double gpuTime = getMillis(t3, t4);
+        printf("Great job :)\n");
+        printf("CPU Time: %.1fms\n", cpuTime);
+        printf("GPU Time: %.1fms\n", gpuTime);
+    }
+
+    // release the memory of x and y
+    free(x);
+    free(y);
+    // release the memory of d_x and d_y
+    cudaFree(d_x);
+    cudaFree(d_y);
+    return 0;
+}
---
+++ ex2_solution.cu
@@ -0,0 +1,175 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<iostream>
+#include<fstream>
+
+#define TILE_WIDTH 32
+
+#define WIDTH 2000
+#define HEIGHT 2000
+
+#define MAX_RGB 256
+
+#define MAX_ITERATIONS 500
+
+using namespace std; // bad practice, you didn't see this
+
+void fillValues(double* c, double endRange, double minVal, double maxVal) {
+    for(int i = 0; i < endRange; i++) {
+        c[i] = (i / endRange) * (maxVal - minVal) + minVal;
+    }
+}
+
+double getMillis(time_t t1, time_t t2) {
+    return (double)(t2 - t1) * 1000.0L;
+}
+
+// our struct representing a color
+typedef struct {
+    unsigned int red;
+    unsigned int green;
+    unsigned int blue;
+} RGB;
+
+/*typedef struct {
+    RGB *image;
+    unsigned int width;
+    unsigned int height;
+} Image;*/
+
+__global__ void drawMandelbrot(RGB *rgb, double *cr, double *ci) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row > HEIGHT || col > WIDTH) {
+        return;
+    }
+
+    int i = 0;
+
+    double zr = 0.0;
+    double zi = 0.0;
+
+    while(i < MAX_ITERATIONS && zr * zr + zi * zi < 4.0) {
+        double fz = zr * zr - zi * zi + cr[col];
+        zi = 2.0 * zr * zi + ci[row];
+        zr = fz;
+        i++;
+    }
+
+// code below only influences coloring, feel free to play around with it
+    int r,g,b;
+/* this will make the set appear only blue
+
+    // comment this line for a green set instead
+    i = (int)((i / MAX_ITERATIONS) * MAX_RGB * MAX_RGB * MAX_RGB);
+
+    b = i / (MAX_RGB * MAX_RGB);
+    int tmp = i - b * MAX_RGB * MAX_RGB;
+    r = tmp / MAX_RGB;
+    g = tmp - r * MAX_RGB;
+*/
+
+	int max = MAX_RGB * MAX_RGB * MAX_RGB;
+	double t = (double)i / MAX_ITERATIONS;
+	i = (int)(t * (double)max);
+	b = i / (MAX_RGB * MAX_RGB);
+	int nn = i - b * MAX_RGB * MAX_RGB;
+	r = nn / MAX_RGB;
+	g = nn - r * MAX_RGB;
+
+    int idx = row * WIDTH + col;
+    rgb[idx].red = r;
+    rgb[idx].green = g;
+    rgb[idx].blue = b;
+}
+
+int main(int argc, char** argv) {
+
+    time_t t1 = clock();
+
+    //Image image;
+    //image.height = HEIGHT;
+    //image.width = WIDTH;
+    RGB *image = (RGB*) malloc(sizeof(RGB) * HEIGHT * WIDTH);
+
+    double minR = -2.0;
+    double maxR = 1.0;
+    double minI = -1.5;
+    double maxI = 1.5;
+
+    double *cr = (double*) malloc(sizeof(double) * WIDTH);
+    double *ci = (double*) malloc(sizeof(double) * HEIGHT);
+
+    fillValues(cr, WIDTH, minR, maxR);
+    fillValues(ci, HEIGHT, minI, maxI);
+
+    RGB *d_rgb;
+    double *d_cr;
+    double *d_ci;
+    
+    cudaError_t status;
+    status = cudaMalloc((void**)&d_rgb, sizeof(RGB) * HEIGHT * WIDTH);
+    status = cudaMalloc((void**)&d_cr, sizeof(double) * WIDTH);
+    status = cudaMalloc((void**)&d_ci, sizeof(double) * HEIGHT);
+
+    if (status != cudaSuccess) {
+        printf("Something went wrong allocating memory\n");
+    }
+
+    status = cudaMemcpy(d_cr, cr, sizeof(double) * HEIGHT, cudaMemcpyHostToDevice);
+    status = cudaMemcpy(d_ci, ci, sizeof(double) * HEIGHT, cudaMemcpyHostToDevice);
+
+    if (status != cudaSuccess) {
+        printf("Something went wrong copying memory to device\n");
+    }
+
+    time_t t2 = clock();
+
+    printf("Allocating memory took %.2fms\n", getMillis(t1, t2));
+
+    int blocks_x = (WIDTH + TILE_WIDTH - 1) / TILE_WIDTH;
+    int blocks_y = (HEIGHT + TILE_WIDTH - 1) / TILE_WIDTH;
+    dim3 grid(blocks_x, blocks_y, 1);
+    dim3 block(TILE_WIDTH, TILE_WIDTH, 1);
+    drawMandelbrot<<<grid, block>>>(d_rgb, d_cr, d_ci);
+    time_t t3 = clock();
+    status = cudaGetLastError();
+    if (status != cudaSuccess) {
+        printf("Kernel did not launch correctly\n");
+    }
+    status = cudaDeviceSynchronize();
+    if (status != cudaSuccess) {
+        printf("cudaDeviceSynchronize returned error code %d\n", status);
+    }
+    cudaMemcpy(image, d_rgb, sizeof(RGB) * HEIGHT * WIDTH, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_rgb);
+    cudaFree(d_cr);
+    cudaFree(d_ci);
+
+    time_t t4 = clock();
+    
+    printf("Drawing image took %.2fms\n", getMillis(t3, t4));
+
+    time_t t5 = clock();
+    ofstream fout("output_image.ppm");
+    fout << "P3" << endl;
+    fout << WIDTH << " " << HEIGHT << endl;
+    fout << MAX_RGB << endl;
+
+    for (int h = 0; h < HEIGHT; h++) {
+        for (int w = 0; w < WIDTH; w += 1) {
+            int i = h * WIDTH + w;
+            fout << image[i].red << " " << image[i].green << " " << image[i].blue << " ";
+       }
+        fout << endl;
+    }
+    fout.close();
+    time_t t6 = clock();
+
+    printf("Writing image to disk took %.2fms\n", getMillis(t5, t6));
+
+    return 0;
+}
